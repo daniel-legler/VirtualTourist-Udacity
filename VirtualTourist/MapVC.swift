27e8c90a600a16a902d5110684a98b2d828fa157
Daniel Legler <legler@Computer.local>
2017-07-01 05:58:06 -0700

    Initial Storyboard commit

---
+++ VirtualTourist/MapVC.swift
@@ -0,0 +1,17 @@
+//
+//  MapVC.swift
+//  VirtualTourist
+//
+//  Created by Daniel Legler on 7/1/17.
+//  Copyright © 2017 Daniel Legler. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapVC: UIViewController, MKMapViewDelegate {
+
+    
+    
+    
+}
