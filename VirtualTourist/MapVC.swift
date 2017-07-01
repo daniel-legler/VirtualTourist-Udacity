01af84e04c1b0b4b65a6d15fb46ed1696aea93cf
Daniel Legler <legler@Computer.local>
2017-07-01 06:32:43 -0700

    Location model added. MapVC code started

--- VirtualTourist/MapVC.swift
+++ VirtualTourist/MapVC.swift
@@ -8,10 +8,47 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class MapVC: UIViewController, MKMapViewDelegate {
 
+    // Will need to load existing annotation points from CoreData
+    // Will need to add annotations when map is long pressed
+    @IBOutlet weak var mapView: MKMapView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "ShowImages" {
+            
+        }
+    }
+    
+    @IBAction func addPin(_ sender: UILongPressGestureRecognizer) {
+        
+        if sender.state == .began {
+            
+            let mapTouchPoint = sender.location(in: mapView)
+            
+            let newAnnotation = MKPointAnnotation()
+            
+            newAnnotation.coordinate = mapView.convert(mapTouchPoint, toCoordinateFrom: mapView)
+            
+            mapView.addAnnotation(newAnnotation)
+            
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        
+        // Look up coordinate in CoreData, prepare for segue to start loading photos on next page, and go to photo view
+            //        view.annotation?.coordinate
+        
+    }
     
 }
